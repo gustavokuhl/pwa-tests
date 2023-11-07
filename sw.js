f309f43e252e9438c3c28d97d313fb16b3702971
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-11-07 18:14:00 -0300

    feat: adiciona funcionamento offline

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
-console.log("I turned myself in a Service Worker, Morty! BOOOOM!!! Big review, I'm a Service Worker!")
+const urlsToCache = ["/index.html", "styles.css"]
+
+self.addEventListener("install", event => {
+  console.log("Service worker instalado")
+  event.waitUntil(
+    async () => {
+      const cache = await caches.open("pwa-assets")
+      return cache.addAll(urlsToCache)
+    }
+  )
+})
+
+self.addEventListener("activate", event => {
+  console.log("Service Worker ativado")
+})
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    (async () => {
+      const match = await caches.match(event.request)
+      return match || fetch(event.request)
+    })()
+  )
+})
